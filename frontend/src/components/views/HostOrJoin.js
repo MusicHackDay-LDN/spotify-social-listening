be25eba0fdc6ef321f11289f289b622b47227236
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-08 13:04:15 +0100

    Add link to party

--- frontend/src/components/views/HostOrJoin.js
+++ frontend/src/components/views/HostOrJoin.js
@@ -3,12 +3,20 @@ import Page from '../layout/Page'
 import { Link } from 'react-router-dom'
 import { GradientButton, Button } from '../layout/Button'
 import { PartyNameInput } from './CreateParty'
+import { withState } from 'recompose'
 
-export default () => (
+const withCode = withState('code', 'updateCode', '')
+
+export default withCode(({ code, updateCode }) => (
   <Page>
     <div>
-      <PartyNameInput mb={2} placeholder="party code" />
-      <Button f={2} mb={3}>
+      <PartyNameInput
+        mb={2}
+        placeholder="party code"
+        value={code}
+        onChange={e => updateCode(e.target.value)}
+      />
+      <Button f={2} mb={3} onClick={() => (window.location = `/party/${code}`)}>
         Join a Party
       </Button>
     </div>
@@ -17,4 +25,4 @@ export default () => (
       <Link to="/create">Host a Party</Link>
     </div>
   </Page>
-)
+))
