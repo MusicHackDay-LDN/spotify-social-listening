cafa75fb5d0d8061a820ba2245a1dc88f8e66d2b
Patrick Arminio <patrick.arminio@gmail.com>
2018-04-08 13:00:42 +0100

    Add party guest mutations

--- frontend/src/components/views/PartyGuest.js
+++ frontend/src/components/views/PartyGuest.js
@@ -1,9 +1,78 @@
 import React from 'react'
 import Page from '../layout/Page'
 import { Button, Input } from 'rebass'
+import gql from 'graphql-tag'
 
-export default () => (
+import { Query, Mutation } from 'react-apollo'
+
+const UPVOTE = gql`
+  mutation Upvote($code: Int!) {
+    upvoteCurrentSong(code: $code) {
+      activeTrack {
+        name
+      }
+      currentUpvotes
+      currentDownvotes
+    }
+  }
+`
+const DOWNVOTE = gql`
+  mutation DownVote($code: Int!) {
+    downvoteCurrentSong(code: $code) {
+      activeTrack {
+        name
+      }
+      currentUpvotes
+      currentDownvotes
+    }
+  }
+`
+
+const GET_PARTY = gql`
+  query Party($code: Int!) {
+    getParty(code: $code) {
+      activeTrack {
+        name
+      }
+      currentUpvotes
+      currentDownvotes
+    }
+  }
+`
+
+const Upvote = ({ code }) => (
+  <Mutation mutation={UPVOTE} variables={{ code }}>
+    {upvote => <div onClick={upvote}>upvote!</div>}
+  </Mutation>
+)
+
+const Downvote = ({ code }) => (
+  <Mutation mutation={DOWNVOTE} variables={{ code }}>
+    {downvote => <div onClick={downvote}>downvote!</div>}
+  </Mutation>
+)
+
+export default props => (
   <Page>
-    <h1>Party</h1>
+    <Query
+      query={GET_PARTY}
+      variables={{ code: props.match.params.partyId }}
+      pollInterval={1000}
+    >
+      {({ loading, error, data, startPolling }) => {
+        if (loading) return 'Loading...'
+        if (error) return `Error! ${error.message}`
+
+        return (
+          <React.Fragment>
+            {data.getParty.activeTrack.name}
+            <div>{data.getParty.currentUpvotes}</div>
+            <div>{data.getParty.currentDownvotes}</div>
+            <Upvote code={props.match.params.partyId} />
+            <Downvote code={props.match.params.partyId} />
+          </React.Fragment>
+        )
+      }}
+    </Query>
   </Page>
 )
